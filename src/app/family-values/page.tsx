141e62a68b35b2e659bb25ef62e54fb300a84c5f
Michael <michaelleie0ca@gmail.com>
2024-01-16 16:53:23 -0800

    Add state to manage the families values and state to control the form input

--- src/app/family-values/page.tsx
+++ src/app/family-values/page.tsx
@@ -1,11 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
 export default function FamilyValuesPage() {
+  const [value, setValue] = useState<string>("");
+  const [values, setValues] = useState<Set<string>>(new Set([]));
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (value.trim().length <= 2) return;
+    setValues((values) => values.add(value));
+    setValue("");
+  };
+
+  const handleDelete = (value: string) => {
+    setValues((values) => {
+      values.delete(value);
+      return new Set(values);
+    });
+  };
+
   return (
     <main className="max-w-4xl mx-auto mt-5">
-      <h1 className="text-3xl font-bold">What are your family values?</h1>
-      <form className="p-5">
-        <input type="text" placeholder="Value" />
-        <button type="submit">Add</button>
+      <form onSubmit={handleSubmit} className="space-y-5">
+        <h1 className="text-3xl font-bold">What are your family values?</h1>
+        <Input
+          type="text"
+          placeholder="Value"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button type="submit" className="w-full">
+          Add
+        </Button>
       </form>
+      <ul className="space-y-5 mt-5">
+        {Array.from(values).map((value) => (
+          <li
+            key={value}
+            className="flex items-center justify-between border p-5"
+          >
+            <p>{value}</p>
+            <Button variant="destructive" onClick={() => handleDelete(value)}>
+              Delete
+            </Button>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
