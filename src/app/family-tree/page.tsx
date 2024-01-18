ccac92044d0f23a2ba24f1e7458c604b869a4dc4
scottchen98 <scott.chen1117@gmail.com>
2024-01-17 23:52:26 -0800

    refactor user data

--- src/app/family-tree/page.tsx
+++ src/app/family-tree/page.tsx
@@ -3,6 +3,7 @@
 import f3 from "family-chart";
 
 import { useRef, useEffect } from "react";
+import familyMembers from "@/app/family-tree/data.json";
 
 export default function FamilyTree() {
   const cont = useRef(null);
@@ -11,7 +12,7 @@ export default function FamilyTree() {
     if (!cont.current) return;
 
     const store = f3.createStore({
-        data: data(),
+        data: familyMembers,
         node_separation: 250,
         level_separation: 150,
       }),
@@ -43,40 +44,6 @@ export default function FamilyTree() {
     view.setCard(Card);
     store.setOnUpdate((props: any) => view.update(props || {}));
     store.update.tree({ initial: true });
-
-    function data() {
-      return [
-        {
-          id: "9ad270aa-a486-41ef-a123-ace2b407dd4e",
-          data: {
-            gender: "M",
-            firstName: "Ben",
-            lastName: "Simmon",
-            birthday: "11/11/1111",
-            avatar: "",
-            placeOfBirth: "Vancouver",
-          },
-          rels: {
-            father: "71ad1ed8-ef8b-4558-a1a1-7681b1e026d2",
-          },
-        },
-        {
-          id: "71ad1ed8-ef8b-4558-a1a1-7681b1e026d2",
-          data: {
-            gender: "M",
-            firstName: "Jeff",
-            lastName: "Simmon",
-            birthday: "22/22/2222",
-            avatar: "",
-            placeOfBirth: "Toronto",
-          },
-          rels: {
-            children: ["9ad270aa-a486-41ef-a123-ace2b407dd4e"],
-            spouses: [],
-          },
-        },
-      ];
-    }
   }, []);
 
   return <div className="f3" id="FamilyChart" ref={cont}></div>;
