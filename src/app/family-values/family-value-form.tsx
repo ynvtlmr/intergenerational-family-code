f43c839937473eb62c15eabed68733adf4593901
Michael <michaelleie0ca@gmail.com>
2024-01-23 21:01:45 -0800

    Add input to capture description of the family value
    - The description will provide context on what the family value means to the family

--- src/app/family-values/family-value-form.tsx
+++ src/app/family-values/family-value-form.tsx
@@ -1,18 +1,26 @@
+"use client";
+
 import { useState } from "react";
 import { useFamilyValues } from "./family-value-store";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function FamilyValueForm() {
   const [value, setValue] = useState<string>("");
-  const { addFamilyValue } = useFamilyValues();
+  const [description, setDescription] = useState<string>("");
+  const { values, addFamilyValue } = useFamilyValues();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (value.trim().length <= 2) return;
-    addFamilyValue(value);
+    if (description.trim().length <= 2) return;
+    if (values[value]) return;
+
+    addFamilyValue({ value, description });
     setValue("");
+    setDescription("");
   };
   return (
     <form onSubmit={handleSubmit} className="space-y-5">
@@ -23,7 +31,12 @@ export default function FamilyValueForm() {
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
-      <Button type="submit" className="w-full">
+      <Textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Enter a short description..."
+      />
+      <Button type="submit" size="lg" className="w-full">
         Add
       </Button>
     </form>
--- src/app/family-values/family-value-list.tsx
+++ src/app/family-values/family-value-list.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useFamilyValues } from "./family-value-store";
 import { Trash2Icon } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -9,16 +10,16 @@ export default function FamilyValueList() {
     deleteFamilyValue(value);
   };
   return (
-    <ul className="space-y-5 mt-5">
-      {Array.from(values).map((value) => (
-        <li
-          key={value}
-          className="flex items-center justify-between border rounded-lg p-5"
-        >
-          <p className="text-xl">{value}</p>
-          <Button variant="destructive" onClick={() => handleDelete(value)}>
-            <Trash2Icon size={24} />
-          </Button>
+    <ul className="space-y-5 mt-5 mb-10">
+      {Object.keys(values).map((value) => (
+        <li key={value} className="border rounded-lg p-5">
+          <h2 className="text-xl">{value}</h2>
+          <p className="text-gray-500 dark:text-gray-400">{values[value]}</p>
+          <div className="flex justify-end mt-5">
+            <Button variant="destructive" onClick={() => handleDelete(value)}>
+              <Trash2Icon size={24} />
+            </Button>
+          </div>
         </li>
       ))}
     </ul>
