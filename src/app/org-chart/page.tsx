cc34e3ae848231abce9f7f1615d56862576f49bc
Michael Lei <michaelleie0ca@gmail.com>
2024-01-31 20:59:42 -0800

    Fix merge conflict with organization chart

--- src/app/org-chart/page.tsx
+++ src/app/org-chart/page.tsx
@@ -1,14 +1,44 @@
 import { OrgChartNode } from "../type";
 import { OrganizationChart } from "primereact/organizationchart";
 
-type OrgChartPageProps = {
-  data: OrgChartNode[];
-};
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
-export default function OrgChartPage({ data }: OrgChartPageProps) {
+const initialData: OrgChartNode[] = [
+  {
+    id: 1,
+    label: "Stark Industries",
+    children: [
+      {
+        id: 2,
+        label: "Stark Corporation",
+        children: [
+          { id: 5, label: "Stark Bank" },
+          { id: 6, label: "Stark Hospital" },
+        ],
+      },
+      {
+        id: 3,
+        label: "Peter Park Corporation",
+        children: [
+          { id: 7, label: "Peter Parker Bank " },
+          { id: 8, label: "Peter Parker Hospitals" },
+        ],
+      },
+      {
+        id: 4,
+        label: "Tony Stark Corporation",
+        children: [{ id: 9, label: "Tony Bank" }],
+      },
+    ],
+  },
+];
+
+export default function OrgChartPage() {
   return (
     <div>
-      <OrganizationChart value={data} />
+      <OrganizationChart value={initialData} />
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-"use client";
-import { useState } from "react";
-import { OrgChartNode } from "./type";
-import OrgChartPage from "./org-chart/page";
-import "primereact/resources/themes/saga-blue/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
-
-const initialData: OrgChartNode[] = [
-  {
-    id: 1,
-    label: "Stark Industries",
-    children: [
-      {
-        id: 2,
-        label: "Stark Corporation",
-        children: [
-          { id: 5, label: "Stark Bank" },
-          { id: 6, label: "Stark Hospital" },
-        ],
-      },
-      {
-        id: 3,
-        label: "Peter Park Corporation",
-        children: [
-          { id: 7, label: "Peter Parker Bank " },
-          { id: 8, label: "Peter Parker Hospitals" },
-        ],
-      },
-      {
-        id: 4,
-        label: "Tony Stark Corporation",
-        children: [{ id: 9, label: "Tony Bank" }],
-      },
-    ],
-  },
-];
-
-export default function Home() {
-  const [orgChartInitialData, setOrgChartInitialData] =
-    useState<OrgChartNode[]>(initialData);
-
-  return (
-    <>
-      <div className=" flex justify-center my-5">
-        <h1 className=" text-4xl font-bold">This is the Org Chart</h1>
-      </div>
-      <div>
-        <OrgChartPage data={orgChartInitialData} />
-      </div>
-    </>
-=======
 import "./globals.css";
 
 import Link from "next/link";
@@ -78,6 +25,5 @@ links.map((link) => (
   ))}
       </div>
     </main>
->>>>>>> 8425f0c3bc30b56a99b1bec4703d24f2dc3caff8
   );
 }
