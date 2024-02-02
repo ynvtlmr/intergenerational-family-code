7d243dbf6287412047453a3573b8f41c4213afa0
Michael <michaelleie0ca@gmail.com>
2024-02-02 15:49:05 -0800

    Fix export with policy tree type error

--- src/app/policy-tree/page.tsx
+++ src/app/policy-tree/page.tsx
@@ -1,33 +1,32 @@
+import React from "react";
+import { DataTable } from "../../components/ui/data-table";
+import { User, columns } from "./columns";
 
-
-import React from 'react';
-import { DataTable } from '../../components/ui/data-table';
-import { User, columns } from './columns';
-
-export const policyData: User[] = [
-        {
-          id: "1",
-          Carrier: "Sun Life",
-          Type: "Par Whole Life",
-          Amount: 100000000,
-          Owner: "Eterna",
-          Beneficiary: "Eterna",
-          Payor: "Eterna",
-          Anniversary: "2026-01-01",
-          Insured: "Dustin Stark",
-        },
-        {
-          id: "2",
-          Carrier: "Manulife",
-          Type: "Par Whole Life",
-          Amount: 100000000,
-          Owner: "Eterna",
-          Beneficiary: "Eterna",
-          Payor: "Eterna",
-          Anniversary: "2026-01-01",
-          Insured: "Dustin Stark",
-        },
-      {  id: "3",
+const policyData: User[] = [
+  {
+    id: "1",
+    Carrier: "Sun Life",
+    Type: "Par Whole Life",
+    Amount: 100000000,
+    Owner: "Eterna",
+    Beneficiary: "Eterna",
+    Payor: "Eterna",
+    Anniversary: "2026-01-01",
+    Insured: "Dustin Stark",
+  },
+  {
+    id: "2",
+    Carrier: "Manulife",
+    Type: "Par Whole Life",
+    Amount: 100000000,
+    Owner: "Eterna",
+    Beneficiary: "Eterna",
+    Payor: "Eterna",
+    Anniversary: "2026-01-01",
+    Insured: "Dustin Stark",
+  },
+  {
+    id: "3",
     Carrier: "Canada Life",
     Type: "Par Whole Life",
     Amount: 100000000,
@@ -92,7 +91,7 @@ export const policyData: User[] = [
     Anniversary: "2028-01-01",
     Insured: "April Stark",
   },
-  
+
   {
     id: "9",
     Carrier: "Sun Life",
@@ -126,17 +125,15 @@ export const policyData: User[] = [
     Anniversary: "2028-01-01",
     Insured: "April Stark",
   },
-      ];
+];
 
-      const policyComponent: React.FC = () => {
-        return (
-          <section className="py-24">
-            <div className='container'>
-              <h1 className='text-center text-5xl font-bold mb-8'>Policy Tree</h1>
-              <DataTable columns={columns} data={policyData} />
-            </div>
-          </section>
-        );
-      };
-      
-      export default policyComponent;
+export default function PolicyComponent() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="mb-8 text-center text-5xl font-bold">Policy Tree</h1>
+        <DataTable columns={columns} data={policyData} />
+      </div>
+    </section>
+  );
+}
