2a73a3ddbc28ec6559e34d4791d9b4a4faedd6e9
Jerry Fan <Jerryfan71@gmail.com>
2024-02-02 16:38:53 -0800

    add a rel attribute in the a tag

--- src/app/family-crest/family-crest-preview.tsx
+++ src/app/family-crest/family-crest-preview.tsx
@@ -19,10 +19,10 @@ export default function FamilyCrestPreview() {
 
   return (
     <div>
-      <a href={crest} target="_blank">
+      <a href={crest} target="_blank" rel="noopener noreferrer">
         <img src={crest} alt="Family Crest" className="mb-4 w-[448px]" />
       </a>
-      <a href={crest} target="_blank">
+      <a href={crest} target="_blank" rel="noopener noreferrer">
         <Button className="w-full">Download</Button>
       </a>
     </div>
