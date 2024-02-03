456b7f83673e712300c3eca99f6714de5c1caad0
Michael <michaelleie0ca@gmail.com>
2024-02-02 18:09:46 -0800

    Refactor test cases for Family Code and Decision Tree pages
    Co-authored-by: Scott Chen <scott.chen1117@gmail.com>

--- src/app/decision-tree/decision-tree.test.tsx
+++ src/app/decision-tree/decision-tree.test.tsx
@@ -1,78 +1,17 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import DecisionTreePage from "./page";
-import userEvent from "@testing-library/user-event";
-
-describe("Decision Tree", () => {
-  test("Should render the textarea and add button on intial load", () => {
-    renderDecisionTreePage();
-
-    const textAreaEl = screen.getByPlaceholderText(
-      "Does this align with our family values?"
-    );
-
-    const buttonEl = screen.getByRole("button", { name: /Add/i });
-
-    expect(textAreaEl).toBeInTheDocument();
-    expect(buttonEl).toBeInTheDocument();
-  });
-
-  test("Should add question to the decision tree", async () => {
-    const user = userEvent.setup();
-    renderDecisionTreePage();
-
-    const textAreaEl = await screen.findByPlaceholderText(
-      "Does this align with our family values?"
-    );
-
-    const question = "Family Question 1";
-
-    await user.type(textAreaEl, question);
-
-    expect(textAreaEl).toHaveValue(question);
-
-    const buttonEl = await screen.findByRole("button", { name: /Add/i });
-
-    await user.click(buttonEl);
-
-    const questionEl = await screen.findByText(question);
-
-    expect(questionEl).toBeInTheDocument();
-
-    const ul = await screen.findByRole("list");
-
-    expect(ul).toContainElement(questionEl);
-  });
-
-  test("Should delete question from the decision tree", async () => {
-    const user = userEvent.setup();
-    renderDecisionTreePage();
-
-    const textAreaEl = await screen.findByPlaceholderText(
-      "Does this align with our family values?"
-    );
-
-    const question = "Family Question 1";
-
-    await user.type(textAreaEl, question);
-
-    const buttonEl = await screen.findByRole("button", { name: /Add/i });
-
-    await user.click(buttonEl);
-
-    const questionEl = await screen.findByText(question);
-
-    expect(questionEl).toBeInTheDocument();
-
-    const deleteButton = await screen.findByTitle("Delete");
-
-    expect(deleteButton).toBeInTheDocument();
-
-    await user.click(deleteButton);
-
-    expect(questionEl).not.toBeInTheDocument();
-  });
-});
+import testFormPageInputOutput from "../__tests__/shared";
 
 const renderDecisionTreePage = () => {
   return render(<DecisionTreePage />);
 };
+
+testFormPageInputOutput({
+  testGroupLabel: "Decision Tree",
+  renderPage: renderDecisionTreePage,
+  initialLoadTestDescription:
+    "Should render the textarea and add button on intial load",
+  inputPlaceholder: "Does this align with our family values?",
+  addItemTestDescription: "Should add question to the decision tree",
+  deleteItemTestDescription: "Should delete question from the decision tree",
+});
--- src/app/family-code/family-code.test.tsx
+++ src/app/family-code/family-code.test.tsx
@@ -1,75 +1,18 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import FamilyCodePage from "./page";
-import userEvent from "@testing-library/user-event";
-
-const placeholder =
-  "The Stark family commits to excellence in that which is most impactful.";
-
-describe("Family Code", () => {
-  test("Should render the textarea and add button on intial load", () => {
-    renderFamilyCodePage();
-
-    const textAreaEl = screen.getByPlaceholderText(placeholder);
-
-    const buttonEl = screen.getByRole("button", { name: /Add/i });
-
-    expect(textAreaEl).toBeInTheDocument();
-    expect(buttonEl).toBeInTheDocument();
-  });
-
-  test("Should add statement to the family code", async () => {
-    const user = userEvent.setup();
-    renderFamilyCodePage();
-
-    const textAreaEl = await screen.findByPlaceholderText(placeholder);
-
-    const statement = "Family Code 1";
-
-    await user.type(textAreaEl, statement);
-
-    expect(textAreaEl).toHaveValue(statement);
-
-    const buttonEl = await screen.findByRole("button", { name: /Add/i });
-
-    await user.click(buttonEl);
-
-    const statementEl = await screen.findByText(statement);
-
-    expect(statementEl).toBeInTheDocument();
-
-    const ul = await screen.findByRole("list");
-
-    expect(ul).toContainElement(statementEl);
-  });
-
-  test("Should delete statement from the family code", async () => {
-    const user = userEvent.setup();
-    renderFamilyCodePage();
-
-    const textAreaEl = await screen.findByPlaceholderText(placeholder);
-
-    const question = "Family Code 1";
-
-    await user.type(textAreaEl, question);
-
-    const buttonEl = await screen.findByRole("button", { name: /Add/i });
-
-    await user.click(buttonEl);
-
-    const statementEl = await screen.findByText(question);
-
-    expect(statementEl).toBeInTheDocument();
-
-    const deleteButton = await screen.findByTitle("Delete");
-
-    expect(deleteButton).toBeInTheDocument();
-
-    await user.click(deleteButton);
-
-    expect(statementEl).not.toBeInTheDocument();
-  });
-});
+import testFormPageInputOutput from "../__tests__/shared";
 
 const renderFamilyCodePage = () => {
   return render(<FamilyCodePage />);
 };
+
+testFormPageInputOutput({
+  testGroupLabel: "Family Code",
+  renderPage: renderFamilyCodePage,
+  initialLoadTestDescription:
+    "Should render the textarea and add button on intial load",
+  inputPlaceholder:
+    "The Stark family commits to excellence in that which is most impactful.",
+  addItemTestDescription: "Should add statement to the family code",
+  deleteItemTestDescription: "Should delete statement from the family code",
+});
