7510a3f779de6fe5a4f4df63f9b04083bb309f2e
scottchen98 <scott.chen1117@gmail.com>
2024-02-06 10:03:44 -0800

    create geAllJson function

--- src/app/file-process.tsx
+++ src/app/file-process.tsx
@@ -1,6 +1,28 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 
+const jsonKeys = [
+  "decision-tree",
+  "family-value",
+  "family-code",
+  "family-vision",
+  "family-tree",
+];
+
 export default function FileProcess() {
+  // Get all the JSON data from localStorage and return it as an object
+  const getAllJson = () => {
+    return jsonKeys.reduce((acc, key) => {
+      let d = localStorage.getItem(key);
+      const jsonData = d ? JSON.parse(d) : "";
+      return {
+        ...acc,
+        [key]: jsonData,
+      };
+    }, {});
+  };
+
   return (
     <div className="mx-auto mt-2 flex max-w-xl justify-center gap-2">
       <Button>Download JSON</Button>
