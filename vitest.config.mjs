778503a7b6b28333245c9fa15272e1126cc823e3
Michael Lei <michaelleie0ca@gmail.com>
2024-02-06 10:32:04 -0800

    Take off vitest globals

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,
     environment: "jsdom",
     setupFiles: ["./vitest-setup.ts"],
   },
