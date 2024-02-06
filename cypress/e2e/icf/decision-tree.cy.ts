7e048c5eace3b3842fa69df62b263ea0b25a342f
Michael Lei <michaelleie0ca@gmail.com>
2024-02-06 11:39:08 -0800

    Add tests for decision tree

--- cypress/e2e/icf/decision-tree.cy.ts
+++ cypress/e2e/icf/decision-tree.cy.ts
@@ -17,11 +17,9 @@ describe("decision", () => {
   it("should delete question from the page when user clicks on the delete button", () => {
     cy.get("textarea").type("Testing decision tree");
     cy.get("form.space-y-5 > .inline-flex").contains("Add").click();
-    cy.getByData("form-item").eq(0).contains("Testing decision tree");
-    cy.get(".lucide-trash2").click();
-    cy.getByData("form-item")
-      .eq(0)
-      .contains("Testing decision tree")
-      .should("not.exist");
+    let formItem = cy.getByData("form-item").eq(0);
+    formItem.contains("Testing decision tree");
+    cy.get(".lucide-trash2").eq(0).click();
+    formItem.should("not.exist");
   });
 });
