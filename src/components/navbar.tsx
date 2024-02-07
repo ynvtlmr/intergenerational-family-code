89026cda611afae4dbed433eee58777afe576ee7
Michael Lei <michaelleie0ca@gmail.com>
2024-02-06 18:45:19 -0800

    Add styles to current active url

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
 import FileProcess from "@/app/file-process";
+import { usePathname } from "next/navigation";
 
 const links = [
   { href: "/decision-tree", label: "Decision Tree" },
@@ -16,12 +19,16 @@ const links = [
 ];
 
 export default function NavBar() {
+  const pathname = usePathname();
   return (
     <header className="flex h-dvh max-w-xs flex-col items-center justify-center border-r p-10">
       <h1 className="mb-8 min-w-0 text-4xl font-bold">ICF</h1>
       <nav className="flex flex-col justify-center gap-2">
         {links.map((link) => (
-          <Button key={link.label}>
+          <Button
+            key={link.label}
+            variant={pathname === link.href ? "default" : "ghost"}
+          >
             <Link className="w-full" href={link.href}>
               {link.label}
             </Link>
