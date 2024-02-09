a0dea23db468537d2666a1dcaa09d149302306b2
scottchen98 <scott.chen1117@gmail.com>
2024-02-09 13:54:31 -0800

    add click and drag interactivity

--- src/app/family-tree/family-tree-flow.tsx
+++ src/app/family-tree/family-tree-flow.tsx
@@ -1,28 +1,57 @@
 "use client";
 
-import ReactFlow, { Controls, Background } from "reactflow";
+import ReactFlow, {
+  Controls,
+  Background,
+  applyEdgeChanges,
+  applyNodeChanges,
+  NodeChange,
+  EdgeChange,
+} from "reactflow";
 import "reactflow/dist/style.css";
 
-export default function FamilyTreeFlow() {
-  const edges = [{ id: "1-2", source: "1", target: "2", label: "to the" }];
+import { useState, useCallback } from "react";
 
-  const nodes = [
-    {
-      id: "1",
-      position: { x: 0, y: 0 },
-      data: { label: "Hello" },
-      type: "input",
-    },
-    {
-      id: "2",
-      position: { x: 100, y: 100 },
-      data: { label: "World" },
-    },
-  ];
+const initialNodes = [
+  {
+    id: "1",
+    data: { label: "Hello" },
+    position: { x: 0, y: 0 },
+    type: "input",
+  },
+  {
+    id: "2",
+    data: { label: "World" },
+    position: { x: 100, y: 100 },
+  },
+];
+
+const initialEdges = [{ id: "1-2", source: "1", target: "2", label: "to the" }];
+
+export default function FamilyTreeFlow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
 
   return (
     <div style={{ height: "100%" }}>
-      <ReactFlow nodes={nodes} edges={edges}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        fitView
+      >
         <Background />
         <Controls />
       </ReactFlow>
