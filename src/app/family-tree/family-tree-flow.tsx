fc65b82c144dbae45922a4f89317f0c3453942d4
scottchen98 <scott.chen1117@gmail.com>
2024-02-09 13:13:25 -0800

    add nodes

--- src/app/family-tree/family-tree-flow.tsx
+++ src/app/family-tree/family-tree-flow.tsx
@@ -4,9 +4,23 @@ import ReactFlow, { Controls, Background } from "reactflow";
 import "reactflow/dist/style.css";
 
 export default function FamilyTreeFlow() {
+  const nodes = [
+    {
+      id: "1",
+      position: { x: 0, y: 0 },
+      data: { label: "Hello" },
+      type: "input",
+    },
+    {
+      id: "2",
+      position: { x: 100, y: 100 },
+      data: { label: "World" },
+    },
+  ];
+
   return (
     <div style={{ height: "100%" }}>
-      <ReactFlow>
+      <ReactFlow nodes={nodes}>
         <Background />
         <Controls />
       </ReactFlow>
