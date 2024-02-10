cbc159cd5211c179d8ff97fbed82fd6f7ee55998
Jerry Fan <Jerryfan71@gmail.com>
2024-02-09 23:46:44 -0800

    Add CreateNodeDialog component with form and button

--- src/app/org-chart/create-node-dialog.tsx
+++ src/app/org-chart/create-node-dialog.tsx
@@ -10,7 +10,41 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Plus } from "lucide-react";
 
 export default function CreateNodeDialog() {
-  return <div>CreateNodeDialog</div>;
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <Plus />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create a Node</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="node-label" className="text-right">
+              Label
+            </Label>
+            <Input
+              id="node-label"
+              defaultValue="Stark enterprises"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button
+            type="submit"
+            className="w-full rounded bg-black p-2 text-white"
+          >
+            Add
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
