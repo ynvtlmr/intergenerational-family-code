622fee5ed6c4e33889b41df6433b6abba76b6069
Jerry Fan <Jerryfan71@gmail.com>
2024-02-09 17:04:48 -0800

    Add TextUpdaterNode component and update OrgChartFlow component

---
+++ src/app/org-chart/node-text-update.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { Divide } from "lucide-react";
+import { useCallback } from "react";
+import { Handle, Position } from "reactflow";
+
+interface TextUpdaterNodeProps {
+  data: any;
+  isConnectable: boolean;
+}
+
+interface handleStyle {
+  left?: number;
+}
+
+const handleStyle = { left: 30 };
+
+export default function TextUpdaterNode({
+  data,
+  isConnectable,
+}: TextUpdaterNodeProps) {
+  const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+  }, []);
+
+  return (
+    <div className="h-12 rounded-md border border-gray-200 bg-white p-1 ">
+      <Handle
+        type="target"
+        position={Position.Top}
+        isConnectable={isConnectable}
+      />
+      <div>
+        <label htmlFor="text" className="block text-xs text-gray-500">
+          Text:
+        </label>
+        <input id="text" name="text" onChange={onChange} className="" />
+      </div>
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        id="a"
+        style={handleStyle}
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        id="b"
+        isConnectable={isConnectable}
+      />
+    </div>
+  );
+}
--- src/app/org-chart/org-chart-flow.tsx
+++ src/app/org-chart/org-chart-flow.tsx
@@ -1,12 +1,65 @@
 "use client";
 
-import ReactFlow, { Controls, Background } from "reactflow";
+import { useState, useCallback } from "react";
+import ReactFlow, {
+  Controls,
+  Background,
+  applyEdgeChanges,
+  applyNodeChanges,
+  addEdge,
+  Edge,
+  Connection,
+} from "reactflow";
 import "reactflow/dist/style.css";
 
+import TextUpdaterNode from "./node-text-update";
+
+const initialNodes = [
+  {
+    id: "node-1",
+    type: "textUpdater",
+    position: { x: 0, y: 0 },
+    data: { value: 123 },
+  },
+];
+
+// const initialEdges = [
+//   { id: "1-2", source: "1", target: "2", label: "to the", type: "step" },
+//   { id: "2-3", source: "2", target: "3", label: "connected" },
+// ];
+
+const nodeTypes = { textUpdater: TextUpdaterNode };
+
 export default function OrgChartFlow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState([]);
+
+  const onNodesChange = useCallback(
+    (changes: any) => setNodes((node) => applyNodeChanges(changes, node)),
+    [setNodes]
+  );
+
+  const onEdgesChange = useCallback(
+    (changes: any) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+
+  const onConnect = useCallback(
+    (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
   return (
     <div style={{ height: "100%" }}>
-      <ReactFlow>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        fitView
+      >
         <Background />
         <Controls />
       </ReactFlow>
