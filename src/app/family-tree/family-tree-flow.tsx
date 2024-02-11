4add659899fcdaf45e6a9be0b5293060d3c346f8
scottchen98 <scott.chen1117@gmail.com>
2024-02-10 19:42:13 -0800

    refactor: create custom useAddNodeOnEdgeDrop and useSaveAndRestore hooks

--- src/app/family-tree/family-tree-flow.tsx
+++ src/app/family-tree/family-tree-flow.tsx
@@ -3,19 +3,14 @@
 import ReactFlow, {
   Controls,
   Background,
-  addEdge,
-  Edge,
-  Connection,
   Panel,
   useNodesState,
   useEdgesState,
-  useReactFlow,
-  OnConnectStartParams,
 } from "reactflow";
 import "reactflow/dist/style.css";
 
-import { useCallback, useRef } from "react";
-import { useToast } from "@/components/ui/use-toast";
+import { useRef } from "react";
+import { useAddNodeOnEdgeDrop, useSaveAndRestore } from "./hooks";
 
 const initialNodes = [
   {
@@ -31,94 +26,14 @@ const getId = () => `${id++}`;
 
 export default function FamilyTreeFlow() {
   const reactFlowWrapper = useRef(null);
-  const connectingNodeId = useRef<string | null>(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const { screenToFlowPosition, setViewport, toObject } = useReactFlow();
-  const { toast } = useToast();
-
-  const onConnect = useCallback(
-    (params: Edge | Connection) => {
-      // reset the start node on connections
-      connectingNodeId.current = null;
-      setEdges((eds) => addEdge(params, eds));
-    },
-    [setEdges]
+  const { onConnect, onConnectStart, onConnectEnd } = useAddNodeOnEdgeDrop(
+    setEdges,
+    setNodes,
+    getId
   );
-
-  const onConnectStart = useCallback(
-    (
-      _: React.MouseEvent | React.TouchEvent,
-      { nodeId }: OnConnectStartParams
-    ) => {
-      connectingNodeId.current = nodeId;
-    },
-    []
-  );
-
-  const onConnectEnd = useCallback(
-    (event: MouseEvent | TouchEvent) => {
-      if (!connectingNodeId.current || !event.target) return;
-
-      // check if the event is a MouseEvent and the target is a pane
-      if (
-        !(event instanceof MouseEvent) ||
-        !(event.target instanceof HTMLDivElement)
-      )
-        return;
-
-      const targetIsPane = event.target.classList.contains("react-flow__pane");
-      if (targetIsPane) {
-        // we need to remove the wrapper bounds, in order to get the correct position
-        const id = getId();
-        const newNode = {
-          id,
-          position: screenToFlowPosition({
-            x: event.clientX,
-            y: event.clientY,
-          }),
-          data: { label: `Node ${id}` },
-          origin: [0.5, 0.0],
-        };
-
-        setNodes((nds) => nds.concat(newNode));
-
-        setEdges((eds) => {
-          // if we are not connecting a node, we don't need to create an edge
-          if (!connectingNodeId.current) return eds;
-
-          return eds.concat({
-            id,
-            source: connectingNodeId.current,
-            target: id,
-          });
-        });
-      }
-    },
-    [setNodes, setEdges, screenToFlowPosition]
-  );
-
-  const onSave = useCallback(() => {
-    // creates a JSON-compatible representation of the flow
-    const flow = toObject();
-    localStorage.setItem("family-tree", JSON.stringify(flow));
-    toast({ title: "Your flow has been saved." });
-  }, [toObject, toast]);
-
-  const onRestore = useCallback(() => {
-    const restoreFlow = async () => {
-      const data = localStorage.getItem("family-tree");
-      const flow = data ? JSON.parse(data) : null;
-      if (flow) {
-        const { x = 0, y = 0, zoom = 1 } = flow.viewport;
-        setNodes(flow.nodes || []);
-        setEdges(flow.edges || []);
-        setViewport({ x, y, zoom });
-      }
-    };
-
-    restoreFlow();
-  }, [setNodes, setEdges, setViewport]);
+  const { onSave, onRestore } = useSaveAndRestore(setNodes, setEdges);
 
   return (
     <div
--- src/app/family-tree/hooks.ts
+++ src/app/family-tree/hooks.ts
@@ -1,28 +1,122 @@
-import { useCallback, useState } from "react";
+import {
+  Connection,
+  Node,
+  Edge,
+  OnConnectStartParams,
+  addEdge,
+  useReactFlow,
+} from "reactflow";
 
-type CopiedValue = string | null;
-type CopyFn = (text: string) => Promise<boolean>; // Return success
+import { Dispatch, SetStateAction, useCallback, useRef } from "react";
+import { useToast } from "@/components/ui/use-toast";
 
-export function useCopyToClipboard(): [CopiedValue, CopyFn] {
-  const [copiedText, setCopiedText] = useState<CopiedValue>(null);
+export function useAddNodeOnEdgeDrop(
+  setEdges: Dispatch<SetStateAction<Edge<any>[]>>,
+  setNodes: Dispatch<SetStateAction<Node<any, string | undefined>[]>>,
+  getId: () => string
+) {
+  const connectingNodeId = useRef<string | null>(null);
+  const { screenToFlowPosition } = useReactFlow();
 
-  const copy: CopyFn = useCallback(async (text) => {
-    if (!navigator?.clipboard) {
-      console.warn("Clipboard not supported");
-      return false;
-    }
+  const onConnect = useCallback(
+    (params: Edge | Connection) => {
+      // reset the start node on connections
+      connectingNodeId.current = null;
+      setEdges((eds) => addEdge(params, eds));
+    },
+    [setEdges]
+  );
 
-    // Try to save to clipboard then save it in the state if worked
-    try {
-      await navigator.clipboard.writeText(text);
-      setCopiedText(text);
-      return true;
-    } catch (error) {
-      console.warn("Copy failed", error);
-      setCopiedText(null);
-      return false;
-    }
-  }, []);
+  const onConnectStart = useCallback(
+    (
+      _: React.MouseEvent | React.TouchEvent,
+      { nodeId }: OnConnectStartParams
+    ) => {
+      connectingNodeId.current = nodeId;
+    },
+    []
+  );
 
-  return [copiedText, copy];
+  const onConnectEnd = useCallback(
+    (event: MouseEvent | TouchEvent) => {
+      if (!connectingNodeId.current || !event.target) return;
+
+      // check if the event is a MouseEvent and the target is a pane
+      if (
+        !(event instanceof MouseEvent) ||
+        !(event.target instanceof HTMLDivElement)
+      )
+        return;
+
+      const targetIsPane = event.target.classList.contains("react-flow__pane");
+      if (targetIsPane) {
+        // we need to remove the wrapper bounds, in order to get the correct position
+        const id = getId();
+        const newNode = {
+          id,
+          position: screenToFlowPosition({
+            x: event.clientX,
+            y: event.clientY,
+          }),
+          data: { label: `Node ${id}` },
+          origin: [0.5, 0.0],
+        };
+
+        setNodes((nds) => nds.concat(newNode));
+
+        setEdges((eds) => {
+          // if we are not connecting a node, we don't need to create an edge
+          if (!connectingNodeId.current) return eds;
+
+          return eds.concat({
+            id,
+            source: connectingNodeId.current,
+            target: id,
+          });
+        });
+      }
+    },
+    [setNodes, setEdges, screenToFlowPosition, getId]
+  );
+
+  return {
+    onConnect,
+    onConnectStart,
+    onConnectEnd,
+  };
+}
+
+export function useSaveAndRestore(
+  setNodes: Dispatch<SetStateAction<Node<any, string | undefined>[]>>,
+  setEdges: Dispatch<SetStateAction<Edge<any>[]>>
+) {
+  const { setViewport, toObject } = useReactFlow();
+  const { toast } = useToast();
+
+  const onSave = useCallback(() => {
+    // creates a JSON-compatible representation of the flow
+    const flow = toObject();
+    localStorage.setItem("family-tree", JSON.stringify(flow));
+    toast({ title: "Your flow has been saved." });
+  }, [toObject, toast]);
+
+  const onRestore = useCallback(() => {
+    const restoreFlow = async () => {
+      const data = localStorage.getItem("family-tree");
+      const flow = data ? JSON.parse(data) : null;
+      if (flow) {
+        const { x = 0, y = 0, zoom = 1 } = flow.viewport;
+        setNodes(flow.nodes || []);
+        setEdges(flow.edges || []);
+        setViewport({ x, y, zoom });
+      }
+    };
+
+    restoreFlow();
+  }, [setNodes, setEdges, setViewport]);
+
+  return {
+    onSave,
+    onRestore,
+  };
 }
