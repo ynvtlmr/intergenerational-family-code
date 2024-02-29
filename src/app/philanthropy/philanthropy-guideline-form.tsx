14493e9585653cb460fcc1d4f2c246ca26c898b6
Michael Lei <michaelleie0ca@gmail.com>
2024-02-29 15:41:18 -0800

    Increase max character count from 50 to 250

--- src/app/philanthropy/philanthropy-guideline-form.tsx
+++ src/app/philanthropy/philanthropy-guideline-form.tsx
@@ -21,8 +21,8 @@ const philanthropyGuidelineFormSchema = z.object({
     .min(2, {
       message: "Guideline must be greater than 2 characters.",
     })
-    .max(50, {
-      message: "Guideline must be less than 50 characters.",
+    .max(250, {
+      message: "Guideline must be less than 250 characters.",
     }),
 });
 type PhilanthropyGuidelineFormSchema = z.infer<
