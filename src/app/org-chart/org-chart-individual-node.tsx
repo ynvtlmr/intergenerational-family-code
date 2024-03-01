da977457c47180365cd030ced328adcc79d81a83
Jerry Fan <Jerryfan71@gmail.com>
2024-02-29 19:14:22 -0800

    Refactor OrgChartIndividualNode component deleting the Tooltip

--- src/app/org-chart/org-chart-individual-node.tsx
+++ src/app/org-chart/org-chart-individual-node.tsx
@@ -3,11 +3,6 @@ import type { Node, NodeProps } from "reactflow";
 import { useDebouncedCallback } from "use-debounce";
 
 import type { NodeOrgData } from "./types";
-import {
-  Tooltip,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 
 export default function OrgChartIndividualNode({
   id,
@@ -40,69 +35,63 @@ export default function OrgChartIndividualNode({
   );
 
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <div className=" rounded-lg bg-zinc-300 p-5 ">
-            <Handle
-              type="target"
-              id="top"
-              position={Position.Bottom}
-              isConnectable={isConnectable}
-            />
-            <Handle
-              type="target"
-              id="left"
-              position={Position.Left}
-              isConnectable={true}
-              onConnect={(params) => {
-                if (params.sourceHandle === "right") {
-                  const edge = {
-                    ...params,
-                    type: "straight",
-                    sourceHandle: "right",
-                  };
-                  setEdges((edges) => addEdge(edge, edges));
-                }
-              }}
-            />
+    <div className=" rounded-lg bg-zinc-300 p-5 ">
+      <Handle
+        type="target"
+        id="top"
+        position={Position.Bottom}
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type="target"
+        id="left"
+        position={Position.Left}
+        isConnectable={true}
+        onConnect={(params) => {
+          if (params.sourceHandle === "right") {
+            const edge = {
+              ...params,
+              type: "straight",
+              sourceHandle: "right",
+            };
+            setEdges((edges) => addEdge(edge, edges));
+          }
+        }}
+      />
 
-            <div className="grid grid-cols-2 gap-5">
-              <input
-                className="min-w-6 text-ellipsis rounded bg-transparent font-semibold placeholder:text-[#eaf7ff] focus:outline-none"
-                name="title"
-                placeholder="Title"
-                defaultValue={title}
-                onChange={onChange}
-              />
-              <input
-                className="min-w-6 text-ellipsis rounded bg-transparent font-semibold placeholder:text-[#eaf7ff] focus:outline-none"
-                name="description"
-                placeholder="Description"
-                defaultValue={description}
-                onChange={onChange}
-              />
-            </div>
+      <div className="grid grid-cols-2 gap-5">
+        <input
+          className="min-w-6 text-ellipsis rounded bg-transparent font-semibold placeholder:text-[#eaf7ff] focus:outline-none"
+          name="title"
+          placeholder="Title"
+          defaultValue={title}
+          onChange={onChange}
+        />
+        <input
+          className="min-w-6 text-ellipsis rounded bg-transparent font-semibold placeholder:text-[#eaf7ff] focus:outline-none"
+          name="description"
+          placeholder="Description"
+          defaultValue={description}
+          onChange={onChange}
+        />
+      </div>
 
-            <Handle
-              type="target"
-              id="right"
-              position={Position.Right}
-              isConnectable={true}
-              onConnect={(params) => {
-                if (params.sourceHandle === "left") {
-                  const edge = {
-                    ...params,
-                    type: "straight",
-                    sourceHandle: "left",
-                  };
-                  setEdges((edges) => addEdge(edge, edges));
-                }
-              }}
-            />
-          </div>
-        </TooltipTrigger>
-      </Tooltip>
-    </TooltipProvider>
+      <Handle
+        type="target"
+        id="right"
+        position={Position.Right}
+        isConnectable={true}
+        onConnect={(params) => {
+          if (params.sourceHandle === "left") {
+            const edge = {
+              ...params,
+              type: "straight",
+              sourceHandle: "left",
+            };
+            setEdges((edges) => addEdge(edge, edges));
+          }
+        }}
+      />
+    </div>
   );
 }
