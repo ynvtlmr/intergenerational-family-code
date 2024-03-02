9d16b27bcf287d6bf9e6047b8b16648b20b08af7
Jerry Fan <Jerryfan71@gmail.com>
2024-03-01 16:48:47 -0800

    commented create-dialog-form and custom-update-node

--- src/app/org-chart/create-node-dialog-hook-form.tsx
+++ src/app/org-chart/create-node-dialog-hook-form.tsx
@@ -1,92 +1,91 @@
-"use client";
+// "use client";
 
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+// import { z } from "zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+// import {
+//   Dialog,
+//   DialogContent,
+//   DialogHeader,
+//   DialogTitle,
+//   DialogTrigger,
+// } from "@/components/ui/dialog";
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Plus } from "lucide-react";
-import { useOrgStore } from "./store";
+// import { Button } from "@/components/ui/button";
+// import { Input } from "@/components/ui/input";
+// import { Plus } from "lucide-react";
 
-const formSchema = z.object({
-  title: z
-    .string()
-    .min(2, { message: "Title Must be at least 2 characters long" })
-    .max(50),
-});
+// const formSchema = z.object({
+//   title: z
+//     .string()
+//     .min(2, { message: "Title Must be at least 2 characters long" })
+//     .max(50),
+// });
 
-export default function CreateNodeDialog() {
-  const addNode = useOrgStore((state) => state.addNode);
+// export default function CreateNodeDialog() {
+//   const addNode = useOrgStore((state) => state.addNode);
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-    },
-  });
+//   const form = useForm<z.infer<typeof formSchema>>({
+//     resolver: zodResolver(formSchema),
+//     defaultValues: {
+//       title: "",
+//     },
+//   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    const nextNode = {
-      id: String(Math.random()),
-      data: { label: values.title },
-      type: "textUpdater",
-      position: { x: 0, y: 0 },
-    };
-    addNode(nextNode);
-    console.log("Node added", nextNode);
-    form.reset();
-  }
+//   function onSubmit(values: z.infer<typeof formSchema>) {
+//     const nextNode = {
+//       id: String(Math.random()),
+//       data: { label: values.title },
+//       type: "textUpdater",
+//       position: { x: 0, y: 0 },
+//     };
+//     addNode(nextNode);
+//     console.log("Node added", nextNode);
+//     form.reset();
+//   }
 
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline" className="rounded-xl">
-          <Plus className="h-4 w-4" />
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Create a Node</DialogTitle>
-        </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Create</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Stark Industries" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className="w-full">
-              Add
-            </Button>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
-  );
-}
+//   return (
+//     <Dialog>
+//       <DialogTrigger asChild>
+//         <Button variant="outline" className="rounded-xl">
+//           <Plus className="h-4 w-4" />
+//         </Button>
+//       </DialogTrigger>
+//       <DialogContent className="sm:max-w-[425px]">
+//         <DialogHeader>
+//           <DialogTitle>Create a Node</DialogTitle>
+//         </DialogHeader>
+//         <Form {...form}>
+//           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+//             <FormField
+//               control={form.control}
+//               name="title"
+//               render={({ field }) => (
+//                 <FormItem>
+//                   <FormLabel>Create</FormLabel>
+//                   <FormControl>
+//                     <Input placeholder="Stark Industries" {...field} />
+//                   </FormControl>
+//                   <FormMessage />
+//                 </FormItem>
+//               )}
+//             />
+//             <Button type="submit" className="w-full">
+//               Add
+//             </Button>
+//           </form>
+//         </Form>
+//       </DialogContent>
+//     </Dialog>
+//   );
+// }
