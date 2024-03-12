cafabfdd435bdf399d422c31a82d517fead805d9
Michael Lei <michaelleie0ca@gmail.com>
2024-03-12 16:01:58 -0700

    Add page component
    Co-authored-by: Scott Chen <scott.chen1117@gmail.com>
    Co-authored-by: Jerry Fan <Jerryfan71@gmail.com>

--- src/app/pdf/page.tsx
+++ src/app/pdf/page.tsx
@@ -1,3 +1,38 @@
+"use client";
+
 export default function PDFPage() {
-  return <main>Data</main>;
+  return (
+    <main className="space-y-10 p-5">
+      <Page title="Decision Tree">
+        <p>Decision Tree content goes here</p>
+      </Page>
+      <Page title="Family Values">
+        <p>Family Values content goes here</p>
+      </Page>
+      <Page title="Family Code">
+        <p>Family Code content goes here</p>
+      </Page>
+      <Page title="Family Vision">
+        <p>Family Code content goes here</p>
+      </Page>
+      <Page title="Philanthropy">
+        <p>Philanthropy content goes here</p>
+      </Page>
+    </main>
+  );
+}
+
+function Page({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="mx-auto max-w-7xl border p-5">
+      <h1 className="mb-3 text-xl font-bold">{title}</h1>
+      {children}
+    </div>
+  );
 }
