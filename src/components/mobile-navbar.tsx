6f7674cb74726a9a41e3866b866f54c76cea12e3
Jerry Fan <Jerryfan71@gmail.com>
2024-03-23 00:24:49 -0700

    Add mobile navbar component

---
+++ src/components/mobile-navbar.tsx
@@ -0,0 +1,12 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+export default function mobileNavbar() {
+  return <div>mobile-navbar</div>;
+}
