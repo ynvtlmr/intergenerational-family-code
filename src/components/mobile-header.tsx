12506d1132558e4fb2c3bfffd602ea6abd22c8b1
Jerry Fan <Jerryfan71@gmail.com>
2024-03-29 12:59:00 -0700

    Add padding to the y-axis in the mobile device

--- src/components/mobile-header.tsx
+++ src/components/mobile-header.tsx
@@ -2,7 +2,7 @@ import MobileSidebar from "./mobile-sidebar";
 
 export default function MobileHeader() {
   return (
-    <nav className=" top-0 flex h-[60px] items-center px-6 lg:hidden">
+    <nav className=" fixed top-0 flex items-center px-6 py-6 lg:hidden">
       <MobileSidebar />
     </nav>
   );
