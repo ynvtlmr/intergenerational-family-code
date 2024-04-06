4ba03e7b02322dbc66c6ae4c9681175e06760952
Michael <michaelleie0ca@gmail.com>
2024-04-05 19:04:02 -0700

    Finish adding contacts to database

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,13 +1,14 @@
 import { ContactForm } from "./contact-form";
 import { Suspense } from "react";
 import Contacts from "./contacts";
+import Loading from "@/components/loading";
 
 export default function ContactPage() {
   return (
     <main className="min-h-screen px-6 pt-5">
       <h1 className="text-3xl font-bold">Contact Form</h1>
       <ContactForm />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loading />}>
         <Contacts />
       </Suspense>
     </main>
