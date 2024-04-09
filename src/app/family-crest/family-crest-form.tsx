f095e0021980a31c19600f9a60bd430274fd0092
Michael Lei <michaelleie0ca@gmail.com>
2024-04-09 16:03:55 -0700

    Update import
    Co-authored-by: Scott Chen <scott.chen1117@gmail.com>

--- src/app/family-crest/family-crest-form.tsx
+++ src/app/family-crest/family-crest-form.tsx
@@ -4,7 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
-import FormSubmitButton from "../../components/form-submit-button";
+import FormSubmitButton from "@/components/form-submit-button";
 import {
   Form,
   FormControl,
