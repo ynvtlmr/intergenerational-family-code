bc752770ee2b4f0ee70c758f5a6a31bf2f605e33
Michael Lei <michaelleie0ca@gmail.com>
2024-04-11 21:51:09 -0700

    Add authentication to more routes

--- src/app/asset-allocation/page.tsx
+++ src/app/asset-allocation/page.tsx
@@ -4,10 +4,14 @@ import { DataTable } from "@/components/data-table";
 import { assetAllocationColumnsWithDelete } from "./columns";
 import AssetPieChart from "./asset-pie-chart";
 import AuthenticatedRoute from "../(auth)/authenticated-route";
+import { redirect } from "next/navigation";
 
 export default async function AssetAllocation() {
   const supabase = createClient();
   const { data } = await supabase.auth.getUser();
+  if (!data?.user) {
+    redirect("/login");
+  }
   const { data: assetAllocations, error } = await supabase
     .from("asset_allocation")
     .select("*")
@@ -22,20 +26,18 @@ export default async function AssetAllocation() {
   }
 
   return (
-    <AuthenticatedRoute>
-      <main className="p-8">
-        <h1 className="mb-4 text-4xl font-bold">Asset Allocation</h1>
-        <AssetAllocationForm />
-        <div className="mt-10">
-          <DataTable
-            columns={assetAllocationColumnsWithDelete}
-            data={assetAllocations}
-          />
-        </div>
-        <div className="mx-auto mt-20 h-96 w-96">
-          <AssetPieChart assetAllocations={assetAllocations} />
-        </div>
-      </main>
-    </AuthenticatedRoute>
+    <main className="p-8">
+      <h1 className="mb-4 text-4xl font-bold">Asset Allocation</h1>
+      <AssetAllocationForm />
+      <div className="mt-10">
+        <DataTable
+          columns={assetAllocationColumnsWithDelete}
+          data={assetAllocations}
+        />
+      </div>
+      <div className="mx-auto mt-20 h-96 w-96">
+        <AssetPieChart assetAllocations={assetAllocations} />
+      </div>
+    </main>
   );
 }
