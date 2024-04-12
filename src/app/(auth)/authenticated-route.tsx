15121d82326b59e51a106b6cd8d7069c4a74de7e
Michael <michaelleie0ca@gmail.com>
2024-04-11 21:13:48 -0700

    Edit authenticated route

--- src/app/(auth)/authenticated-route.tsx
+++ src/app/(auth)/authenticated-route.tsx
@@ -1,6 +1,3 @@
-"use client";
-import { usePathname } from "next/navigation";
-
 type AuthenticatedRouteProps = {
   children: React.ReactNode;
 };
@@ -8,18 +5,6 @@ type AuthenticatedRouteProps = {
 export default function AuthenticatedRoute({
   children,
 }: AuthenticatedRouteProps) {
-  // const { isAuthenticating, user } = useAuth();
-  const pathname = usePathname();
-  // const { push } = useRouter();
-
-  if (
-    pathname === "/login" ||
-    pathname === "/signup" ||
-    pathname === "/verify-email"
-  ) {
-    return <>{children}</>;
-  }
-
   // if (isAuthenticating) {
   //   return <Loading />;
   // }
