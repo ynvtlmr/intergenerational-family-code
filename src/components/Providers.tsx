a63d621f7511fd7f3b8683a0995d545dc16b4c46
Michael <michaelleie0ca@gmail.com>
2024-04-11 21:06:40 -0700

    Remove unecessary auth provider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,13 +2,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
-import { AuthProvider } from "./providers/auth-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>{children}</AuthProvider>
+      {children}
       {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
